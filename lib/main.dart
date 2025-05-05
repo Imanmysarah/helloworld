946fea986c6a10de21c5b73102677e5370fa03ac
Imanmysarah <imanmysarah3108@gmail.com>
2025-05-05 11:29:33 +0800

    Making row/column

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,49 @@
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello World Demo Application',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-      );
-  }
-  }
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 255, 178, 233),
-        appBar: AppBar(
-          backgroundColor: Colors.purpleAccent,
-            title: Text("Home Page"),
-            centerTitle: true,
-      ),
-      body: Center(
-        child:
-            Image(
-              image:NetworkImage('https://img.freepik.com/free-vector/red-beauty-butterfly-insect-icon_18591-82365.jpg?semt=ais_hybrid&w=740'),
-      )),
-    );
-  }
+ void main() => runApp(MyApp());
+ class MyApp extends StatelessWidget {
+ // This widget is the root of your  
+// application.
+ @override
+ Widget build(BuildContext context) {
+ return MaterialApp(
+ title: 'Basic Widget',
+ theme: ThemeData(
+ primarySwatch: Colors.blue,
+ ),
+ home: MyHomePage(),
+ );
+ } 
 }
+ class MyHomePage extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+ return Scaffold(
+ appBar: AppBar(
+ title: Text("Home Page"),
+ backgroundColor: Colors.purpleAccent,
+ centerTitle: true,
+ ),
+  body: Row(
+     mainAxisAlignment: MainAxisAlignment.center,
+     crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+      Container(
+          color: Colors.blue,
+          width: 100,
+          height: 100,
+          child: Center(
+          child: Text('BMW')),
+    ),
+      Container(
+          color: Colors.yellow,
+          width: 100,
+          height: 100,
+          child: Center(
+          child: Text('Ford')),
+        ),
+ ]
+ ),
+ backgroundColor: Colors.white,
+ );
+ }
+ }
